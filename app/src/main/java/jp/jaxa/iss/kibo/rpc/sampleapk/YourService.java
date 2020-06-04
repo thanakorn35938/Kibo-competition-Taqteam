abc477312c8a4d95c79ec6d079747c485ccfab3e
thanakorn35938 <scientan@gmail.com>
2020-06-04 10:03:34 +0700

    V.9

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
@@ -1,8 +1,6 @@
 package jp.jaxa.iss.kibo.rpc.sampleapk;
 
 import android.graphics.Bitmap;
-import android.util.Log;
-import android.view.ViewDebug;
 
 import net.sourceforge.zbar.Config;
 import net.sourceforge.zbar.Image;
@@ -17,52 +15,51 @@ import org.opencv.core.Mat;
 import java.util.ArrayList;
 import java.util.List;
 
-import gov.nasa.arc.astrobee.Kinematics;
 import gov.nasa.arc.astrobee.Result;
 import gov.nasa.arc.astrobee.types.Point;
 import gov.nasa.arc.astrobee.types.Quaternion;
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
 
 public class YourService extends KiboRpcService {
+
     @Override
     protected void runPlan1() {
         api.judgeSendStart();
-        String p1_1_con[] = new String[2];
-        String p1_2_con[] = new String[2];
-        String p1_3_con[] = new String[2];
-        String p2_1_con[] = new String[2];
-        String p2_2_con[] = new String[2];
-        String p2_3_con[] = new String[2];
+        String[] p1_1_con = new String[2];
+        String[] p1_2_con = new String[2];
+        String[] p1_3_con = new String[2];
+        String[] p2_1_con = new String[2];
+        String[] p2_2_con = new String[2];
+        String[] p2_3_con = new String[2];
         int arv = 0;
 
-        String p1_1 = moveToQR(11.49, -5.7, 4.5, 0, 0, 0, 1,0);
-        p1_1_con = p1_1.split(", ");
-        double a = Double.parseDouble(p1_1_con[1]);
-
-        String p1_3 = moveToQR(11, -5.5, 4.36, 0.707, 0, -0.707, 0,2);
+        String p1_3 = moveToQR(11.042f, -5.583f, 4.360f, 0.500f, 0.500f, 0.500f, -0.50, 2);
         p1_3_con = p1_3.split(", ");
         double b = Double.parseDouble(p1_3_con[1]);
 
-        String p1_2 = moveToQR(11, -6, 5.44, 0.5, 0.5, 0.5, -0.5,1);
+        String p1_1 = moveToQR(11.440f, -5.659f, 4.583f, 0.000f, 0.000f, 0.000f, 1.000f, 0);
+        p1_1_con = p1_1.split(", ");
+        double a = Double.parseDouble(p1_1_con[1]);
+
+        String p1_2 = moveToQR(11.083f, -6.450f, 5.262f, 0.000f, 0.000f, 0.000f, -0.500f, 1);
         p1_2_con = p1_2.split(", ");
         double c = Double.parseDouble(p1_2_con[1]);
 
-        moveToWrapper(10.50,-6.45,5.40,0,0,0,0);
+        moveToWrapper(10.500f, -6.450f, 5.262f, 0.000f, 0.000f, 0.000f, 0.000f);
 
-        String p2_2 = moveToQR(11.49,-8,5,0,0,0,1,4);
+        String p2_2 = moveToQR(11.490f, -7.958f, 5.083f, 0.000f, 0.000f, 0.000f, 1.000f, 4);
         p2_2_con = p2_2.split(", ");
         double d = Double.parseDouble(p2_2_con[1]);
 
-        String p2_3 = moveToQR(11,-7.7,5.44,0.7,0,0.7,0 ,5);
+        String p2_3 = moveToQR(11.083f, -7.742f, 5.440f, 0.707f, 0.000f, 0.707f, 0.000f, 5);
         p2_3_con = p2_3.split(", ");
         double e = Double.parseDouble(p2_3_con[1]);
 
-        String p2_1 = moveToQR(10.41,-7.5,4.7,0,0,1,0,3);
+        String p2_1 = moveToQR(10.410f, -7.542f, 4.783f, 0.000f, 0.000f, 1.000f, 0.000f, 3);
         p2_1_con = p2_1.split(", ");
         double f = Double.parseDouble(p2_1_con[1]);
-        //Ar PART
 
-        moveToWrapper(10.95,-9.2,5.35,0,0,0,0);
+        moveToWrapper(10.950f, -9.590f, 5.410f, 0.000f, 0.000f, 0.000f, -0.707f);
 
         Mat ids = new Mat();
         while (arv == 0) {
@@ -73,8 +70,7 @@ public class YourService extends KiboRpcService {
             try {
                 Aruco.detectMarkers(source, dictionary, corners, ids);
                 arv = (int) ids.get(0, 0)[0];
-            }
-            catch (Exception ec) {
+            } catch (Exception ec) {
             }
         }
         api.judgeSendDiscoveredAR(Integer.toString(arv));
@@ -84,8 +80,7 @@ public class YourService extends KiboRpcService {
 
     private void moveToWrapper(double pos_x, double pos_y, double pos_z,
                                double qua_x, double qua_y, double qua_z,
-                               double qua_w)
-    {
+                               double qua_w) {
 
         final int LOOP_MAX = 3;
         final Point point = new Point(pos_x, pos_y, pos_z);
@@ -105,7 +100,7 @@ public class YourService extends KiboRpcService {
 
         int width = bMap.getWidth();
         int height = bMap.getHeight();
-        int pixel[] = new int[width * height];
+        int[] pixel = new int[width * height];
         bMap.getPixels(pixel, 0, width, 0, 0, width, height);
         Image barcode = new Image(width, height, "RGB4");
 
@@ -138,11 +133,11 @@ public class YourService extends KiboRpcService {
         String back = null;
         while (back == null) {
             moveToWrapper(pos_x, pos_y, pos_z, qua_x, qua_y, qua_z, qua_w);
-            Bitmap bmap = api.getBitmapNavCam();
-            back = scanQRImage(bmap);
+            Bitmap img = api.getBitmapNavCam();
+            back = scanQRImage(img);
         }
         api.judgeSendDiscoveredQR(QR, back);
-        Log.d("QR_Reading_Result", QR + "  " + back);
         return back;
     }
+
 }
