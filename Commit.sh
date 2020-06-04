24280c2732e582e1f7e4921de158f1e06e50c891
thanakorn35938 <scientan@gmail.com>
2020-06-04 10:04:09 +0700

    V.9

--- Commit.sh
+++ Commit.sh
@@ -3,4 +3,4 @@ git init
 git add .
 git commit -m "V.9"
 git remote add origin https://github.com/thanakorn35938/Kibo-Competititon-Taqteam.git
-git push -u origin Testament
+git push -u origin master/Testament
