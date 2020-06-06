3fbcaf29815fe0096f7fc7dcab4edf3e9110ba30
Thanakorn35938 <scientan@gmail.com>
2020-06-06 17:07:41 +0700

    Welcom Class a

--- Commit.sh
+++ Commit.sh
@@ -2,5 +2,4 @@ echo # Kibo-Competititon-Taqteam >> README.md
 git init
 git add .
 git commit -m "Welcom Class a"
-git remote add origin https://github.com/thanakorn35938/Kibo-Competititon-Taqteam.git
 git push -u origin master
